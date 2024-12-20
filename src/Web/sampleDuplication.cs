9a93a09bfd6cfe7fa24d824c0d0bbb3c5ccf3820
pratikshatiwari <47111738+pratikshatiwari@users.noreply.github.com>
2024-12-20 14:02:26 +0530

    Create sampleDuplication.cs

---
+++ src/Web/sampleDuplication.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace DuplicationExample
+{
+    public class Example
+    {
+        public void MethodA()
+        {
+            Console.WriteLine("This is a duplicated block.");
+            Console.WriteLine("Performing action A.");
+        }
+
+        public void MethodB()
+        {
+            Console.WriteLine("This is a duplicated block.");
+            Console.WriteLine("Performing action A.");
+        }
+    }
+}
